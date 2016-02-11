db5a61b5b315a154f1fec822549fe25e2c57ecd7
Anthony Grove <agrove87@gmail.com>
2016-02-11 06:36:53 +0000

    add react-router

--- app/app.js
+++ app/app.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
+import { Router, browserHistory } from 'react-router';
 
-import Main from './components/main';
 import reducers from './reducers/reducers';
+import routes from './routes';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Main />
+    <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('#mount'));
--- app/components/main.js
+++ app/components/main.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 export default class Main extends Component {
   render() {
     return (
         <div>
             Hello World!
+            {this.props.children}
         </div>
     );
   }
