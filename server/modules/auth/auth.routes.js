4f56bae52da9b972a8dfa3e5dd63df7889c48c5a
Anthony Grove <agrove87@gmail.com>
2016-06-11 23:15:00 +0000

    (SERVER)(AUTH)(ROUTES) refactor

--- server/modules/auth/auth.routes.js
+++ server/modules/auth/auth.routes.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const auth = require('./auth.controller');
+const { register, login, fbLogin, gLogin, updateEmail, updatePassword, addPassword, createJwt, } = require('./auth.controller');
 const passport = require('passport');
 
 require('./passport-strategies');
@@ -8,23 +8,21 @@ const requireAuth = passport.authenticate('jwt', { session: false, });
 const requireLogin = passport.authenticate('local', { session: false, failWithError: true, });
 
 module.exports = (express, app) => {
-
   const apiAuthRoutes = express.Router();
 
-  apiAuthRoutes.route('/register').post(auth.register, auth.createJwt); //{email, password} > token
+  apiAuthRoutes.route('/register').post(register, createJwt); //{email, password} > token
 
-  apiAuthRoutes.route('/login').post(requireLogin, auth.login, auth.createJwt); //{email, password} > token
+  apiAuthRoutes.route('/login').post(requireLogin, login, createJwt); //{email, password} > token
 
-  apiAuthRoutes.route('/facebook').post(auth.fbLogin, auth.createJwt); //{name, id, acessToken} > token
+  apiAuthRoutes.route('/facebook').post(fbLogin, createJwt); //{name, id, acessToken} > token
 
-  apiAuthRoutes.route('/google').post(auth.gLogin, auth.createJwt); //{name, id, acessToken} > token
+  apiAuthRoutes.route('/google').post(gLogin, createJwt); //{name, id, acessToken} > token
 
-  apiAuthRoutes.route('/update-email').put(requireAuth, auth.updateEmail, auth.createJwt); //{token, password, newEmail} > new Token
+  apiAuthRoutes.route('/update-email').put(requireAuth, updateEmail, createJwt); //{token, password, newEmail} > new Token
 
-  apiAuthRoutes.route('/update-pass').put(requireAuth, auth.updatePassword, auth.createJwt); //{token, oldPassword, newPassword} > sucess
+  apiAuthRoutes.route('/update-pass').put(requireAuth, updatePassword, createJwt); //{token, oldPassword, newPassword} > sucess
 
-  apiAuthRoutes.route('/add-pass').put(requireAuth, auth.addPassword, auth.createJwt); //{token, newPassword} > sucess
+  apiAuthRoutes.route('/add-pass').put(requireAuth, addPassword, createJwt); //{token, newPassword} > sucess
 
   app.use('/api/auth/', apiAuthRoutes);
-
 };
